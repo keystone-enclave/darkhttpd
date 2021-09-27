538004cd6f8f9b577a2bbe8b44ed8df3438fda4f
Dayeol Lee <dayeol@berkeley.edu>
2021-09-27 03:19:37 +0000

    Fix arguments

--- darkhttpd.c
+++ darkhttpd.c
@@ -1359,7 +1359,7 @@ static void log_connection(const struct connection *conn) {
         use_safe(user_agent)
         );
     fflush(logfile);
-  }    
+  }
 #define free_safe(x) if (safe_##x) free(safe_##x)
 
     free_safe(method);
@@ -2801,6 +2801,11 @@ static void stop_running(int sig unused) {
 
 /* Execution starts here. */
 int main(int argc, char **argv) {
+    argc = 2;
+    char* arg_array[2];
+    arg_array[0] = argv[0];
+    arg_array[1] = ".";
+    argv = arg_array;
     printf("%s, %s.\n", pkgname, copyright);
     parse_default_extension_map();
     parse_commandline(argc, argv);
